9d147bedf8555d649ccfea189c5d6c23c9c4553a
Mike Spiegel <mspiegel31@gmail.com>
2024-07-20 11:24:59 -0400

    change baseUrl to fix deploymeny

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://mspiegel31.github.io/mikespiegel/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
