49cf6078104b2745ebd5a65ec279df5207660c55
Mike Spiegel <mspiegel31@gmail.com>
2024-07-20 13:06:15 -0400

    configure edit page

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -40,7 +40,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/mspiegel31/mikespiegel/edit/main/',
         },
         theme: {
           customCss: './src/css/custom.css',
